9e2ca1be5ebef673b75697a245654c84e7c41adc
Usman-Shaikhnag <ushaikhnag@gmail.com>
2025-02-15 00:49:18 +0530

    pull

--- controllers/profile.py
+++ controllers/profile.py
@@ -238,7 +238,7 @@ class ProductController(http.Controller):
         response = request.make_response(excel_buffer.read(),
                                          headers=[
                                              ('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'),
-                                             ('Content-Disposition', 'attachment; filename=product_enquiries.xlsx;')
+                                             ('Content-Disposition', 'attachment; filename=Buyers_Information.xlsx;')
                                          ])
         return response
     
@@ -1031,5 +1031,34 @@ class ProductController(http.Controller):
         partner_id = user.partner_id
         notification = request.env['subscribed.notifications'].sudo().search([('id','=',notification_id),('partner_id.id','=',partner_id.id)])
         notification.sudo().unlink()
+
+
+    @http.route('/download_brochure', type='http', auth='public', website=True)
+    def download_brochure(self, **kwargs):
+
+        # brochure_record = request.env['trademeda.brochure'].sudo().search([('sequence', '=', 1)], limit=1)
+
+        brochure_record = request.env['trademeda.conf'].sudo().search([], limit=1)
+        # import wdb;wdb.set_trace()
+        
+        if brochure_record and brochure_record.brochure_file:
+            # Get the binary content of the brochure
+            pdf_content = brochure_record.brochure_file
+            pdf_content = base64.b64decode(pdf_content)
+
+            # Use the filename provided or default to "brochure.pdf"
+            filename = brochure_record.brochure_filename or "brochure.pdf"
+
+            # Set the headers to serve the file as a downloadable attachment
+            headers = [
+                ('Content-Type', 'application/pdf'),
+                ('Content-Disposition', f'attachment; filename="{filename}"'),
+            ]
+
+            # Return the response
+            return request.make_response(pdf_content, headers=headers)
+        else:
+            # Return a 404 error if no brochure is found
+            return request.not_found()
 
 
